15a4bd5295e9838228d6c710bfb6826f7a04e454
Samir M <67711948+samirm00@users.noreply.github.com>
2023-09-05 16:03:09 +0200

    update loader

--- src/components/createLoader.js
+++ src/components/createLoader.js
@@ -1,12 +1,7 @@
 const createLoader = () => {
-    const container = document.createElement('div');
-    container.className = 'loader-container';
-
     const loader = document.createElement('div');
     loader.className = 'loader';
-
-    container.append(loader);
-    return container;
+    return loader;
 };
 
 export default createLoader;
